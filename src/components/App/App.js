7937c1097e1c8ed8d226caea0d1a0ee2e06af85b
mohamuhsin <muhsinohoho@outlook.com>
2022-09-01 19:12:27 +0300

    Reupdated App name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>React Restaurant</h1>
+        <h1>Ohoho Restaurant</h1>
         <SearchBar />
         <BusinessList />
       </div>
