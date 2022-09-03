cccc41cda597bba50f253f04e18e0855f8bf6bdc
mohamuhsin <muhsinohoho@outlook.com>
2022-09-03 13:31:10 +0300

    Passed information to Components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,15 +6,10 @@ export default class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map((business) => {
+          return <Business business={business} />;
+        })}
       </div>
     );
   }
 }
-
-
